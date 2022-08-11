bc905ad3a1643ff7e35125356e796dc354c7f935
Andre Light <andrelight.al@gmail.com>
2022-08-11 19:40:22 +0000

    Implement file line iterator
    - Would work with files of any size.

--- 01_easy1/05_encrypted_pioneers.rb
+++ 01_easy1/05_encrypted_pioneers.rb
@@ -21,10 +21,56 @@ module Decryption
   end
 end
 
-# Use the Decryption module to patch `String` only through the end of this file.
+class FileLineComparer
+  include Enumerable
+
+  attr_reader :path1_line, :path2_line
+
+  def initialize(path1_line, path2_line)
+    @path1_line = path1_line
+    @path2_line = path2_line
+  end
+
+  def each
+    return to_enum unless block_given?
+
+    File.open(path1_line) do |path1_io|
+      path1_enum = path1_io.each
+      File.open(path2_line) do |path2_io|
+        path2_enum = path2_io.each
+
+        loop do
+          yield(path1_enum.next.chomp, path2_enum.next.chomp)
+        end
+      end
+    end
+  end
+
+  def all_first_transformed_match_second?
+    each do |path1_line, path2_line|
+      transformed = yield(path1_line)
+      return false if transformed != path2_line
+    end
+
+    true
+  end
+end
+
+# Use the Decryption module to patch `String` in a lexically scoped manner
+# so patch is active only through the end of this file.
 using Decryption
 
-input = File.new('05_encrypted_pioneers_input.txt').each.map(&:decrypt_rot13)
-expected_output = File.new('05_encrypted_pioneers_expected_output.txt').to_a
+# Iterate through each file's lines
+puts(FileLineComparer.new(
+  '05_encrypted_pioneers_input.txt',
+  '05_encrypted_pioneers_expected_output.txt'
+).map do |path1_line, path2_line|
+  decrypted = path1_line.decrypt_rot13
+  "Expectation matches decrypted? #{decrypted == path2_line} | #{path1_line} -> #{path2_line}"
+end)
 
-p input == expected_output
+# Even simpler if you just want to transform and verify equality:
+puts FileLineComparer.new(
+  '05_encrypted_pioneers_input.txt',
+  '05_encrypted_pioneers_expected_output.txt'
+).all_first_transformed_match_second?(&:decrypt_rot13)
