0f0b14636297580a2a1654da69725f8d9871a4b1
Andre Light <andrelight.al@gmail.com>
2022-08-12 23:39:32 +0000

    Simplify Easy 1, Exercise 9
    - Counting is unnecessary; clarify intent by using a single boolean.

--- 01_easy1/09_iterators_true_for_one.rb
+++ 01_easy1/09_iterators_true_for_one.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 def one?(enumerable)
-  count = 0
+  seen_one = false
 
   enumerable.each do |*args|
-    count += 1 if yield(*args)
-    return false if count > 1
-  end
-  return true if count == 1
+    next unless yield(*args)
+    return false if seen_one
 
-  false
+    seen_one = true
+  end
+
+  seen_one
 end
 
 p one?([1, 3, 5, 6]) { |value| value.even? } == true
