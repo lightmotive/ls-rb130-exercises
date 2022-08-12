eb4b209b26d3dfc9e781967621b187d6b357f2cf
Andre Light <andrelight.al@gmail.com>
2022-08-12 23:26:32 +0000

    Disable tests when file is `require`ed
    - Will reuse solution in another file.

--- 01_easy1/06_iterators_true_for_any.rb
+++ 01_easy1/06_iterators_true_for_any.rb
@@ -6,6 +6,8 @@ def any?(enumerable)
   false
 end
 
+return unless __FILE__ == $PROGRAM_NAME
+
 p any?([1, 3, 5, 6]) { |value| value.even? } == true
 p any?([1, 3, 5, 7]) { |value| value.even? } == false
 p any?([2, 4, 6, 8]) { |value| value.odd? } == false
