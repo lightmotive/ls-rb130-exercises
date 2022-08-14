1ac2839c206168a286819ab23dbbaea7039467a0
Andre Light <andrelight.al@gmail.com>
2022-08-14 19:03:13 +0000

    Simplify Easy 1, Exercise 10 implementation
    - Increment inline using ternary conditional expression.
    - Simplify unit test.

--- 01_easy1/10_count_items.rb
+++ 01_easy1/10_count_items.rb
@@ -7,9 +7,7 @@ def count(enumerable)
 
   # Alternative solution that doesn't use `each`, `loop`, `while`, or `until`:
   enumerable.reduce(0) do |count, args|
-    count += 1 if yield(*args)
-
-    count
+    count + (yield(*args) ? 1 : 0)
   end
 end
 
@@ -19,6 +17,7 @@ p count([1, 2, 3, 4, 5]) { |_value| true } == 5
 p count([1, 2, 3, 4, 5]) { |_value| false } == 0
 p count([]) { |value| value.even? } == 0
 p count(%w[Four score and seven]) { |value| value.size == 5 } == 2
+# A few more tests to show that it works with hashes or other objects with multi-arg yields
 p count({ k1: 5, k2: 7 }) { |k, v|  k == :k2 && v == 7 } == 1
 p count({ k1: 5, k2: 7 }) { |*args| args == [:k1, 5] } == 1
-p count({}) { |*_args| true }.zero?
+p count({}) { true }.zero?
