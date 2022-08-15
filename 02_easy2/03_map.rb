2f18e04107c0c88e16db560d84a855732df61f6e
Andre Light <andrelight.al@gmail.com>
2022-08-15 23:50:38 +0000

    Explore Easy 2, Exercise 3
    - Modify `map` method so it works with any enumerable argument.

--- 02_easy2/03_map.rb
+++ 02_easy2/03_map.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
-def map(array)
-  array.each_with_object([]) { |e, mapped| mapped << yield(e) }
+require 'set'
+
+def map(enumerable)
+  enumerable.each_with_object([]) { |(*args), mapped| mapped << yield(*args) }
 end
 
 p(map([1, 3, 6]) { |value| value**2 } == [1, 9, 36])
 p(map([]) { |_value| true } == [])
 p(map(%w[a b c d]) { |_value| false } == [false, false, false, false])
-p(map(%w[a b c d]) { |value| value.upcase } == %w[A B C D])
+p(map(%w[a b c d], &:upcase) == %w[A B C D])
 p(map([1, 3, 4]) { |value| (1..value).to_a } == [[1], [1, 2, 3], [1, 2, 3, 4]])
+p(map({ a: 'A', b: 'B' }) { |key, value| [key, value] } == [[:a, 'A'], [:b, 'B']])
+p(map({ a: 'A', b: 'B' }) { |*entry| entry } == [[:a, 'A'], [:b, 'B']])
+p(map(Set[1, 'c', :s]) { |value| [value] } == [[1], ['c'], [:s]])
