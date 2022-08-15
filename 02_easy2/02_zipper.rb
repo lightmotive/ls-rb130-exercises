8da2e08830f39aadb7c5a8dcfa2f8eab9552d931
Andre Light <andrelight.al@gmail.com>
2022-08-15 23:04:28 +0000

    Simplify with **Enumerator#with_object**

--- 02_easy2/02_zipper.rb
+++ 02_easy2/02_zipper.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 def zip(arr1, arr2)
-  zipped = []
-
-  arr1.each_with_index do |e, i|
+  arr1.each_with_index.with_object([]) do |(e, i), zipped|
     zipped << [e, arr2[i]]
   end
-
-  zipped
 end
 
 p zip([1, 2, 3], [4, 5, 6]) == [[1, 4], [2, 5], [3, 6]]
