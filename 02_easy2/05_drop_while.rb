736828f7262a8295a2f10dea7aba0d3171601225
Andre Light <andrelight.al@gmail.com>
2022-08-16 02:12:10 +0000

    Complete Easy 2, Exercise 5

---
+++ 02_easy2/05_drop_while.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+def drop_while(enumerable)
+  keep_searching = true
+  enumerable.each_with_object([]) do |args, keep|
+    next if keep_searching && yield(args)
+
+    keep_searching = false
+
+    keep << args
+  end
+end
+
+p(drop_while([1, 3, 5, 6]) { |value| value.odd? } == [6])
+p(drop_while([1, 3, 5, 6]) { |value| value.even? } == [1, 3, 5, 6])
+p(drop_while([1, 3, 5, 6]) { true } == [])
+p(drop_while([1, 3, 5, 6]) { false } == [1, 3, 5, 6])
+p(drop_while([1, 3, 5, 6]) { |value| value < 5 } == [5, 6])
+p(drop_while([]) { true } == [])
