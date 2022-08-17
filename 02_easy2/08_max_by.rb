036b6918a724bee47f1b423e8098b05c06a0dfe9
Andre Light <andrelight.al@gmail.com>
2022-08-17 22:58:21 +0000

    Complete Easy 2, Exercise 8

---
+++ 02_easy2/08_max_by.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+def max_by(enumerable)
+  max = nil
+  item_max_by = nil
+
+  enumerable.each do |item|
+    compare_value = yield(item)
+    if max.nil? || compare_value > max
+      max = compare_value
+      item_max_by = item
+    end
+  end
+
+  item_max_by
+end
+
+p(max_by([1, 5, 3]) { |value| value + 2 } == 5)
+p(max_by([1, 5, 3]) { |value| 9 - value } == 1)
+p(max_by([1, 5, 3]) { |value| (96 - value).chr } == 1)
+p(max_by([[1, 2], [3, 4, 5], [6]]) { |value| value.size } == [3, 4, 5])
+p(max_by([-7]) { |value| value * 3 } == -7)
+p(max_by([]) { |value| value + 5 }.nil?)
