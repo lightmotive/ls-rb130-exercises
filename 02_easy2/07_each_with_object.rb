daa802d1cb607f4f4b505f2f89518634ab7e6517
Andre Light <andrelight.al@gmail.com>
2022-08-17 22:37:07 +0000

    Complete Easy 2, Exercise 7

---
+++ 02_easy2/07_each_with_object.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+def each_with_object(enumerable, obj)
+  enumerable.each { |item| yield(item, obj) }
+  obj
+end
+
+result = each_with_object([1, 3, 5], []) do |value, list|
+  list << value**2
+end
+p result == [1, 9, 25]
+
+result = each_with_object([1, 3, 5], []) do |value, list|
+  list << (1..value).to_a
+end
+p result == [[1], [1, 2, 3], [1, 2, 3, 4, 5]]
+
+result = each_with_object([1, 3, 5], {}) do |value, hash|
+  hash[value] = value**2
+end
+p result == { 1 => 1, 3 => 9, 5 => 25 }
+
+result = each_with_object([], {}) do |value, hash|
+  hash[value] = value * 2
+end
+p result == {}
