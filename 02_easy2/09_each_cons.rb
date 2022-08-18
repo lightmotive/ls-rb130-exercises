a31318846f9f8e8d8f909c75401c0db877a96358
Andre Light <andrelight.al@gmail.com>
2022-08-18 00:00:26 +0000

    Complete Easy 2, Exercise 10
    - Modify test case so block expects an array arg.
      - `Enumerable#each_cons` always yields an array; mine does the same.
      - Modified line deconstructs the argument.
        - Another option would be to modify the expected hash keys, e.g.:
          `{ [1] => true, ... }`

--- 02_easy2/09_each_cons.rb
+++ 02_easy2/09_each_cons.rb
@@ -37,7 +37,7 @@ p hash == { 'a' => 'b' }
 p result.nil?
 
 hash = {}
-each_cons([1, 3, 6, 10], 1) do |value|
+each_cons([1, 3, 6, 10], 1) do |(value)|
   hash[value] = true
 end
 p hash == { 1 => true, 3 => true, 6 => true, 10 => true }
