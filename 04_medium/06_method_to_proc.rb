edd44ca6ad90b0944e5c91385c370b59df1d2531
Andre Light <andrelight.al@gmail.com>
2022-08-19 23:55:07 +0000

    Simplify Medium 1, Exercise 6
    - `&` before the last arg invokes `to_proc` on the arg.
      - `Method` objects define `to_proc`, so no need to explicitly covert.

--- 04_medium/06_method_to_proc.rb
+++ 04_medium/06_method_to_proc.rb
@@ -4,5 +4,5 @@ def convert_to_base8(n)
   n.to_s(8).to_i
 end
 
-base8_proc = method(:convert_to_base8).to_proc
-p [8, 10, 12, 14, 16, 33].map(&base8_proc) == [10, 12, 14, 16, 20, 41]
+base8_method = method(:convert_to_base8)
+p [8, 10, 12, 14, 16, 33].map(&base8_method) == [10, 12, 14, 16, 20, 41]
