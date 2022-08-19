fc136a52030237a5697422dbf4dcd3d1e2d1dc66
Andre Light <andrelight.al@gmail.com>
2022-08-19 23:48:11 +0000

    Complete Medium 1, Exercise 6

---
+++ 04_medium/06_method_to_proc.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+def convert_to_base8(n)
+  n.to_s(8).to_i
+end
+
+base8_proc = method(:convert_to_base8).to_proc
+p [8, 10, 12, 14, 16, 33].map(&base8_proc) == [10, 12, 14, 16, 20, 41]
