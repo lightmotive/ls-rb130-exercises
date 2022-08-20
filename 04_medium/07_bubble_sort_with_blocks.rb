74029842c193a7729d4e69ded95894fd7d21b6ab
Andre Light <andrelight.al@gmail.com>
2022-08-20 01:25:15 +0000

    Complete Medium 1, Exercise 7
    - Expand algorithm detail.
    - Update test cases to support more intuitive algorithm.

--- 04_medium/07_bubble_sort_with_blocks.rb
+++ 04_medium/07_bubble_sort_with_blocks.rb
@@ -8,13 +8,16 @@
 
 # Flexible block comparison algorithm
 # With two adjacent elements `first` and `second`:
-# - Pass first and second to block with requirement to return what a combined
-#   comparison expression would return (block returns one of the following):
-#   - -1 (first is less than second)
-#   - 0 (elements are equal)
-#   - 1 (first is greater than second)
-# The rest of this algorithm is the same as the fixed comparison algorithm above:
-# - If first <= second:
+# - If no block given:
+#   - comparison_result = first <=> second
+# - else:
+#   - comparison_result = yield(first, second)
+#     - Pass first and second to block with requirement to return what a combined
+#       comparison expression would return (block returns one of the following):
+#       - -1 (first is less than second)
+#       - 0 (elements are equal)
+#       - 1 (first is greater than second)
+# - If comparison_result <= 0:
 #   - Skip
 # - Otherwise, swap associated first and second elements
 
@@ -26,9 +29,13 @@ def bubble_sort!(array)
     swapped = false
 
     0.upto(array.size - last_index_offset) do |idx|
-      next if array[idx] <= array[idx + 1]
+      first = array[idx]
+      second = array[idx + 1]
+      comparison_result = block_given? ? yield(first, second) : first <=> second
+      next if comparison_result <= 0
 
-      array[idx], array[idx + 1] = array[idx + 1], array[idx]
+      array[idx] = second
+      array[idx + 1] = first
       swapped = true
     end
 
@@ -40,24 +47,24 @@ end
 
 array = [5, 3]
 bubble_sort!(array)
-array == [3, 5]
+p array == [3, 5]
 
 array = [5, 3, 7]
-bubble_sort!(array) { |first, second| first >= second }
-array == [7, 5, 3]
+bubble_sort!(array) { |first, second| second <=> first }
+p array == [7, 5, 3]
 
 array = [6, 2, 7, 1, 4]
 bubble_sort!(array)
-array == [1, 2, 4, 6, 7]
+p array == [1, 2, 4, 6, 7]
 
 array = [6, 12, 27, 22, 14]
-bubble_sort!(array) { |first, second| (first % 7) <= (second % 7) }
-array == [14, 22, 12, 6, 27]
+bubble_sort!(array) { |first, second| (first % 7) <=> (second % 7) }
+p array == [14, 22, 12, 6, 27]
 
 array = %w[sue Pete alice Tyler rachel Kim bonnie]
 bubble_sort!(array)
-array == %w[Kim Pete Tyler alice bonnie rachel sue]
+p array == %w[Kim Pete Tyler alice bonnie rachel sue]
 
 array = %w[sue Pete alice Tyler rachel Kim bonnie]
-bubble_sort!(array) { |first, second| first.downcase <= second.downcase }
-array == %w[alice bonnie Kim Pete rachel sue Tyler]
+bubble_sort!(array) { |first, second| first.downcase <=> second.downcase }
+p array == %w[alice bonnie Kim Pete rachel sue Tyler]
