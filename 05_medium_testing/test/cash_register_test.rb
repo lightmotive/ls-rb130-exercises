aa13ab6ca6bf62b78c2f494a9817896c8c37718a
Andre Light <andrelight.al@gmail.com>
2022-08-22 02:19:44 +0000

    Complete Medium Testing, Exercise 3

--- 05_medium_testing/test/cash_register_test.rb
+++ 05_medium_testing/test/cash_register_test.rb
@@ -4,6 +4,25 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 MiniTest::Reporters.use!
 
-class CashRegisterTest < MiniTest::Test
+require './transaction'
+require './cash_register'
 
+class CashRegisterTest < MiniTest::Test
+  def setup
+    @default_item_cost = 1
+    @transaction = Transaction.new(@default_item_cost)
+    @transaction.amount_paid = @default_item_cost
+    @default_total_money = 100
+    @register = CashRegister.new(@default_total_money)
+  end
+
+  def test_initialize
+    assert_equal(@default_total_money, @register.total_money)
+    assert_equal(@default_item_cost, @transaction.amount_paid)
+  end
+
+  def test_accept_money
+    @register.accept_money(@transaction)
+    assert_equal(@default_total_money + @transaction.amount_paid, @register.total_money)
+  end
 end
