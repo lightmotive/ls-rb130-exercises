09ffd95adf0f727886dc11d85c71c77687d83835
Andre Light <andrelight.al@gmail.com>
2022-08-22 02:30:59 +0000

    Improve Medium Testing, Exercise 4
    - Implement `CashRegister#change` method tests.

--- 05_medium_testing/test/cash_register_test.rb
+++ 05_medium_testing/test/cash_register_test.rb
@@ -17,12 +17,22 @@ class CashRegisterTest < MiniTest::Test
 
   def test_initialize
     assert_equal(@default_total_money, @register.total_money)
-    assert_equal(@default_item_cost, @transaction.amount_paid)
+    assert_equal(@default_item_cost, @transaction.item_cost)
   end
 
   def test_accept_money
     @transaction.amount_paid = @default_item_cost
     @register.accept_money(@transaction)
     assert_equal(@default_total_money + @transaction.amount_paid, @register.total_money)
+  end
+
+  def test_change_zero
+    @transaction.amount_paid = @default_item_cost
+    assert_equal(0, @register.change(@transaction))
+  end
+
+  def test_change_positive
+    @transaction.amount_paid = @default_item_cost + 10
+    assert_equal(10, @register.change(@transaction))
   end
 end
