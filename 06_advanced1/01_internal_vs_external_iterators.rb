4c68924ab02ebf0d80a862f600ea9c5f8f6dd562
Andre Light <andrelight.al@gmail.com>
2022-08-24 23:55:14 +0000

    Play with Advanced 1, Exercise 1
    - Expand Enumerable usage.
    - Improve and vary output.

--- 06_advanced1/01_internal_vs_external_iterators.rb
+++ 06_advanced1/01_internal_vs_external_iterators.rb
@@ -13,17 +13,23 @@ end
 
 factorial_enumerator = enum_for(:factorial_iterator)
 
-7.times { puts factorial_enumerator.next }
+puts 'Output first 5 factorials:'
+5.times { puts factorial_enumerator.next }
+puts '2 more:', factorial_enumerator.next, factorial_enumerator.next
 
 puts '[Rewind]'
 factorial_enumerator.rewind
 
-puts factorial_enumerator.next
-puts factorial_enumerator.next
-puts factorial_enumerator.next
+puts
+puts 'Factorials 1-3:'
+3.times { puts factorial_enumerator.next }
 
-puts '[Use Enumerator#first - no need to rewind first]'
-puts factorial_enumerator.first(7)
+puts
+print 'Enumerator#first(7).to_a: '
+p factorial_enumerator.first(7).to_a
 
-puts '[Continue external iteration from last `next` call, before Enumerable#first call]'
-puts factorial_enumerator.next, factorial_enumerator.next, factorial_enumerator.next
+puts
+puts 'Factorials 4-7 (continued from 1-3):'
+4.times { puts factorial_enumerator.next }
+# Interesting that external iteration operates independently Enumerable method
+# calls on the same object.
