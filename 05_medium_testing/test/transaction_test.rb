e6f3250628327b9bd90da9c8eab7d7e9b56d7ee6
Andre Light <andrelight.al@gmail.com>
2022-08-24 02:22:20 +0000

    Complete Medium Testing, Exercise 6

---
+++ 05_medium_testing/test/transaction_test.rb
@@ -0,0 +1,43 @@
+require 'minitest/autorun'
+require './transaction'
+
+class TransactionTest < MiniTest::Test
+  def setup
+    @item_cost = 30
+    @transaction = Transaction.new(@item_cost)
+    @you_owe_stdout = "You owe $#{@item_cost}.\nHow much are you paying?\n"
+    @invalid_amount_stdout = 'That is not the correct amount. ' \
+    "Please make sure to pay the full cost.\n"
+  end
+
+  def test_prompt_for_payment_exact
+    input = StringIO.new("#{@item_cost}\n")
+
+    assert_output(@you_owe_stdout) do
+      @transaction.prompt_for_payment(input: input)
+    end
+    assert_equal(@item_cost, @transaction.amount_paid)
+  end
+
+  def test_prompt_for_payment_invalid
+    input = StringIO.new("-10\n20\n30\n")
+
+    incorrect_amount_stdout_sequence = "#{@invalid_amount_stdout}#{@you_owe_stdout}"
+    expected_stdout = <<~STDOUT.chomp
+      #{@you_owe_stdout.chomp}
+      #{incorrect_amount_stdout_sequence * 2}
+    STDOUT
+
+    assert_output(expected_stdout) do
+      @transaction.prompt_for_payment(input: input)
+    end
+
+    assert_equal(@item_cost, @transaction.amount_paid)
+  end
+
+  def test_prompt_for_payment_over
+    input = StringIO.new("40\n")
+    @transaction.prompt_for_payment(input: input)
+    assert_equal(40, @transaction.amount_paid)
+  end
+end
