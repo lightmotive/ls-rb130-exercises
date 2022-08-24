a327cdb860fba6f80c4236540c96524071d88dc0
Andre Light <andrelight.al@gmail.com>
2022-08-24 22:22:11 +0000

    Complete Medium Testing, Exercise 8
    - Add sample files.

---
+++ 05_medium_testing/text.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Text
+  def initialize(text)
+    @text = text
+  end
+
+  def swap(letter_one, letter_two)
+    @text.gsub(letter_one, letter_two)
+  end
+end
