2b9f46d973a46ce3867c0eff4fc4384fc388f014
Andre Light <andrelight.al@gmail.com>
2022-08-27 02:33:16 +0000

    Complete Ruby Challenge, Easy 1

--- 07_challenges/triangle.rb
+++ 07_challenges/triangle.rb
@@ -5,7 +5,7 @@
 # Rules (implicit & explicit)
 # - sides.length must be 3
 # - All sides must be greater than 0
-# - Sum of any 2 sides must be greater than sum of all sides
+# - Sum of any 2 sides must be greater than the length of the 3rd side.
 #
 # * E *
 # - Tests in test/triangle_test.rb.
@@ -28,19 +28,37 @@ class Triangle
 
   def initialize(*sides)
     @sides = sides
+    validate_sides
+    determine_kind
   end
 
   private
 
   def validate_sides
-    # - sides.length must be 3
-    # - All sides must be greater than 0
-    # - Sum of any 2 sides must be greater than sum of all sides
+    raise ArgumentError, 'Must provide 3 sides as separate arguments.' unless sides.length == 3
+    raise ArgumentError, 'All side lengths must be greater than 0.' unless sides.all?(&:positive?)
+
+    unless side_lengths_valid?
+      raise ArgumentError,
+            'The sum of any 2 side lengths must be greater than the 3rd side length.'
+    end
+  end
+
+  # Sum of any 2 sides must be greater than the length of the 3rd side.
+  def side_lengths_valid?
+    is_sequence_valid = ->(sides) { sides.first(2).sum > sides.last }
+    return false unless is_sequence_valid.call(sides)
+    return false unless is_sequence_valid.call(sides.rotate)
+    return false unless is_sequence_valid.call(sides.rotate)
+
+    true
   end
 
   def determine_kind
-    #   - An equilateral triangle has all three sides the same length.
-    #   - An isosceles triangle has exactly two sides of the same length.
-    #   - A scalene triangle has all sides of different lengths.
+    @kind = case sides.uniq.count
+            when 1 then 'equilateral'
+            when 2 then 'isosceles'
+            when 3 then 'scalene'
+            end
   end
 end
