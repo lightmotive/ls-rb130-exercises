4378f7b1f1ae8c1b95545c2a6c514178ae815138
Andre Light <andrelight.al@gmail.com>
2022-08-28 00:37:54 +0000

    Complete Ruby Challenge, Easy 2

--- 07_challenges/point_mutation.rb
+++ 07_challenges/point_mutation.rb
@@ -11,6 +11,23 @@
 
 # Basic algorithm:
 # - Convert each strand to an array of chars.
-# - Check strand length.
-#   - If different, iterate through the shorter array with index.
-# - Count differences across arrays, comparing each letter at each index.
+# - Iterate through the shortest array with index.
+#   - Count differences across arrays, comparing each letter at each index.
+
+class DNA
+  attr_reader :strand
+
+  def initialize(strand)
+    @strand = strand
+  end
+
+  def hamming_distance(other_strand)
+    first_chars = strand.chars
+    second_chars = other_strand.chars
+    first_chars, second_chars = [first_chars, second_chars].minmax_by(&:size) if first_chars.size != second_chars.size
+
+    first_chars.each_with_index.reduce(0) do |distance, (char, idx)|
+      distance + (char == second_chars[idx] ? 0 : 1)
+    end
+  end
+end
