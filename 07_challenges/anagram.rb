83d2d5eb2fda3043a4557152d1d2ed7b8410c295
Andre Light <andrelight.al@gmail.com>
2022-08-28 22:21:04 +0000

    Process Ruby Challenge, Easy 4
    - Review test cases; add rules that cover additional requirements.

--- 07_challenges/anagram.rb
+++ 07_challenges/anagram.rb
@@ -8,3 +8,8 @@
 # - They are words or phrases that rearrange letters of other words or phrases.
 # - The rearrangements use each letter exactly once.
 # - Ignore spaces in both the input and when checking the output.
+# - Ignore identical words.
+# - Anagrams are case-insensitive.
+
+# Examples, test cases
+# The provided test cases prompted some additional rules above.
