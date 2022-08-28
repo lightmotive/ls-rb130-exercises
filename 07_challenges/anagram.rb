af7354eca569d287dc833a7b7d20177148e1346e
Andre Light <andrelight.al@gmail.com>
2022-08-28 22:19:47 +0000

    Process Ruby Challenge, Easy 4
    - Add basic rules based on Wikipedia article.

---
+++ 07_challenges/anagram.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# The problem
+# Input: string, array of strings (candidates: are they anagrams of `string`?)
+# Output: array of strings that are anagrams of `string`
+#
+# Anagram rules:
+# - They are words or phrases that rearrange letters of other words or phrases.
+# - The rearrangements use each letter exactly once.
+# - Ignore spaces in both the input and when checking the output.
