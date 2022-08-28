0a6c57d0ff44a57ac9196950afa852a6f5b128c7
Andre Light <andrelight.al@gmail.com>
2022-08-28 02:45:53 +0000

    Process Ruby Challenge, Easy 3
    - Test algorithm for basic correctness before implementation.
    - Algorithm is too complicated; try one that looks at each digit.

--- 07_challenges/roman_numeral.rb
+++ 07_challenges/roman_numeral.rb
@@ -34,9 +34,34 @@
 # Given an `integer`:
 # - Initialize `numerals = ''`
 # - remainder = integer
-# - Iterate through NUMERALS as |numeral, unit| (largest to smallest):
+# - Enumerate through NUMERALS as |numeral, unit| (largest to smallest):
+#   - Skip numeral if remainder < unit.
 #   - Divide remainder by unit to get quotient and remainder.
 #     - quotient indicates number of numerals
 #       - If quotient <= 3, append (numeral * quotient) to numerals.
-#       - If quotient == 4, append "#{numeral}#{previous_numeral}".
+#       - Else If quotient > 4, append "#{numeral}#{previous_numeral}".
 #         - quotient should never exceed 4.
+#       - next_remainder = remainder((unit - remainder) / next_unit)
+#         - If next_remainder > 3:
+#           - Convert remainder to...
+# ...this is too complicated. Need an algorithm that looks at each digit...
+# Test algorithm:
+# remainder = 149
+# Enumerate...
+#   ***...skip 1000 and 500...***
+#   *** C, 100 ***
+#   149 / 100 = 1r49
+#   numerals += C
+#   remainder = 49
+#   *** skip L, 50 ***
+#   *** X, 10 ***
+#   49 / 10 = 4r9
+#   numerals += XL => CXL
+#   remainder = 9
+#   *** V, 5 ***
+#   9 / 5 = 1r4
+#   numerals += V => CXLV
+#   remainder = 4
+#   *** I, 1 ***
+#   4 / 1 = 4r0
+#   Need to replace previous letter with
