23a7a7de4eb26599596c36bd7cfd15a563e6c1d0
Andre Light <andrelight.al@gmail.com>
2022-08-28 22:38:39 +0000

    Process Ruby Challenge, Easy 4
    - Data structures and algorithm shell.

--- 07_challenges/anagram.rb
+++ 07_challenges/anagram.rb
@@ -13,3 +13,43 @@
 
 # Examples, test cases
 # The provided test cases prompted some additional rules above.
+
+# Data structures
+# - class: Anagram (shell defined)
+# - Candidates are provided as arrays.
+# - Compare standardized strings.
+
+# Algorithm:
+# Class and basic iteration written first, then fleshed out.
+
+class Anagram
+  attr_reader :string
+
+  def initialize(string)
+    @string = string
+    @string_for_comparison = string_for_comparison(@string)
+  end
+
+  def match(words_or_phrases)
+    anagrams = []
+    # - They are words or phrases that rearrange letters of other words or phrases.
+
+    words_or_phrases.each do |candidate|
+      # - Ignore identical words.
+
+      candidate_for_comparison = string_for_comparison(candidate)
+      # - The rearrangements use each letter exactly once.
+    end
+
+    anagrams
+  end
+
+  private
+
+  attr_reader :string_for_comparison
+
+  def string_for_comparison(string)
+    # - Anagrams are case-insensitive.
+    # - Ignore spaces in both the input and when checking the output.
+  end
+end
