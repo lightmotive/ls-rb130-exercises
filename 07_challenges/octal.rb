ff0a34680a8a4f237600a29b925e156d4e3a0201
Andre Light <andrelight.al@gmail.com>
2022-08-31 02:14:44 +0000

    Process Ruby Challenge, Easy 7
    - Validate algorithm.

--- 07_challenges/octal.rb
+++ 07_challenges/octal.rb
@@ -20,7 +20,15 @@
 # Given an `octal_string` input:
 # - Validate string: `return 0 if octal_string.count(^0-9) > 0`--use helper).
 # - Drop leading zeros.
-# - Reverse string, then enumerate through remaining characters with index.
+# - Reverse string, then enumerate through remaining chars with index.
 #   - Convert the char to an integer.
 #   - Multiply the char by 8^idx.
 #   - Sum all values (reduce).
+#
+# Example:
+# "17" => 15
+# "71"
+# 7 * 8^0 = 7
+# 1 * 8^1 = 8
+# 7 + 8 = 15
+# Valid!
