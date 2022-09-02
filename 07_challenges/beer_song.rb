12fbc73ee882c2b930e100b68d86daa3735e0adb
Andre Light <andrelight.al@gmail.com>
2022-09-02 23:16:52 +0000

    Implement Ruby Challenge, Easy 9
    - Implement `Verse` class.
    - Test single verse output.

--- 07_challenges/beer_song.rb
+++ 07_challenges/beer_song.rb
@@ -1,52 +1,54 @@
 # frozen_string_literal: true
 
 class BeerSong
-  # Private class that encapsulates verse attributes and behaviors/templates.
+  # Private class that encapsulates "verse" attributes and behaviors/templates.
+  # ToDo: Internationalize strings in this class.
   class Verse
     def initialize(number)
       @number = number
     end
 
     def to_s
-      # Generate specific verse with this format:
-      #   "2 bottles of beer on the wall, 2 bottles of beer.\n" \
-      #   "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+      <<~VERSE
+        #{line1}
+        #{line2}
+      VERSE
     end
 
     private
 
     attr_reader :number
 
-    def excerpt_bottles_of_beer(bottle_count)
-      # - Account for plurality:
-      #   "1 bottle of beer on the wall, 1 bottle of beer.\n" \
-      #   "Take it down and pass it around, no more bottles of beer on the wall.\n"
-      # - Account for 0:
-      #   "No more bottles of beer on the wall, no more bottles of beer.\n" \
-      #   "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-      # ...
-      # return `n` bottle(s) of beer
+    def excerpt_bottles(bottle_count, is_sentence_start: false)
+      no_more = is_sentence_start ? 'No more' : 'no more'
+      quantity = bottle_count.zero? ? no_more : bottle_count.to_s
+      bottles_string = bottle_count == 1 ? 'bottle' : 'bottles'
+      "#{quantity} #{bottles_string} of beer"
+    end
+
+    def excerpt_take_down
+      "Take #{number == 1 ? 'it' : 'one'} down"
     end
 
     def line1
-      # "#{bottles_of_beer(number)} on the wall, #{bottles_of_beer(number)}\n"
+      "#{excerpt_bottles(number, is_sentence_start: true)} on the wall, #{excerpt_bottles(number)}."
     end
 
     def line2
-      # - If number.positive?:
-      #   - "Take it down and pass it around, #{bottles_of_beer(number - 1)} on the wall.\n"
-      # - Else:
-      #   - "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+      return "#{excerpt_take_down} and pass it around, #{excerpt_bottles(number - 1)} on the wall." if number.positive?
+
+      "Go to the store and buy some more, #{excerpt_bottles(99)} on the wall."
     end
   end
+
   private_constant :Verse
 
   def self.verse(number)
-    # Use `Verse` class
+    Verse.new(number).to_s
   end
 
   def self.verses(first, last)
-    # generate verses from first..last with an empty line between each.
+    # generate verses from first..last with an empty line between each
   end
 
   def self.lyrics
