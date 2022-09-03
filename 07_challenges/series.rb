a794fd5d07072cf7906d2de2f0c874985b189764
Andre Light <andrelight.al@gmail.com>
2022-09-03 02:34:32 +0000

    Process Ruby Challenge, Easy 10
    - Basic algorithm.

--- 07_challenges/series.rb
+++ 07_challenges/series.rb
@@ -10,6 +10,10 @@ class Series
   def slices(length)
     raise ArgumentError, 'Slice length must be <= integer string length.' if length > integers.length
 
-    # Algorithm...
+    # Algorithm:
+    # - Iterate through the string slices of specified length, incrementing
+    #   the starting position with each iteration. Iterate from
+    #   0..(integers.length - length), taking length of chars from string.
+    #   - Map chars to_i.
   end
 end
