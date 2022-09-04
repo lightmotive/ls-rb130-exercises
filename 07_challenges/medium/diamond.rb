e554c8628018c1fdef56191441e8e4dbbbd2c9cb
Andre Light <andrelight.al@gmail.com>
2022-09-04 19:01:25 +0000

    Implement Ruby Challenge, Medium 1
    - Refactor methods.

--- 07_challenges/medium/diamond.rb
+++ 07_challenges/medium/diamond.rb
@@ -4,20 +4,31 @@
 # - Within basic class structure below.
 
 class Diamond
-  def self.make_diamond(to_letter)
-    # Rules:
-    # - Ensure to_letter length is 1 && to_letter.ord >= 'A'.ord.
-    # - Upcase to_letter.
+  START_LETTER = 'A'
 
-    # * Examples *
-    # - See test/diamond_test.rb
+  attr_reader :end_letter
 
-    # * Data structure *
-    # - Since the halves on either side of the middle are duplicates, store
-    #   the first half output in an array.
+  def initialize(end_letter)
+    @end_letter = end_letter_with_validation(end_letter)
+  end
+
+  def as_string
+    as_lines.join("\n")
+  end
+
+  def self.make_diamond(end_letter)
+    new(end_letter).as_string
+  end
+
+  private
+
+  def as_lines
+    lines_top = []
 
     # * Algorithm *
-    # - Determine diamond width: size of letters from A..to_letter
+    letters = START_LETTER..end_letter
+
+    # - Determine diamond width: size of letters from A..end_letter
     # - Line components:
     #   - Letter(s)
     #   - Space between letters (start at 0 and increase with each line)
@@ -28,7 +39,27 @@ class Diamond
     # - To generate the bottom half: append a reversed the top half of lines,
     #   excluding the middle.
 
-    # Output: diamond starting with A, expanding through to_letter, then
+    # Output: diamond starting with A, expanding through end_letter, then
     # half above middle reversed.
+    lines_bottom = lines_top[0..-2].reverse
+    lines_top + lines_bottom
+  end
+
+  def end_letter_with_validation(input)
+    raise ArgumentError, "Specify a single uppercase letter after #{START_LETTER}" if input.length > 1
+
+    input_validated = input.upcase
+    if input_validated.ord < START_LETTER.ord
+      raise ArgumentError, "Letter must be '#{START_LETTER}' or after on the ASCII char table."
+    end
+
+    input_validated
+  end
+
+  def line(letter, line_length, space_between_letters = nil)
+    return letter.center(line_length) if space_between_letters.nil?
+
+    letters = "#{letter}#{' ' * space_between_letters}#{letter}"
+    letters.center(line_length)
   end
 end
