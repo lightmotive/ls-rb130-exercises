f182a589fb8a4e622e3917b3be496d53a4c13f2d
Andre Light <andrelight.al@gmail.com>
2022-09-11 23:04:41 +0000

    Update regex book exercise 6: add string anchors

--- regex/exercise6.rb
+++ regex/exercise6.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 def format_date(string)
-  string.gsub(/(\d{4})-(\d{2})-(\d{2})/, '\3.\2.\1') # \1: year; \2: month; \3: day
+  string.gsub(/\A(\d{4})-(\d{2})-(\d{2})\z/, '\3.\2.\1') # \1: year; \2: month; \3: day
 end
 
 p format_date('2016-06-17') == '17.06.2016'
