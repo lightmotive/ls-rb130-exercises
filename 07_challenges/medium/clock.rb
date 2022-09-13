d65cc63390b50d26afd31d1c4adf392f93f0dbbc
Andre Light <andrelight.al@gmail.com>
2022-09-13 01:14:58 +0000

    Finish processing Ruby Challenge, Medium 3
    - [PEDA]C. Ready to implement.

--- 07_challenges/medium/clock.rb
+++ 07_challenges/medium/clock.rb
@@ -6,21 +6,36 @@
 # Rules:
 # - Use only arithmetic operations. Don't use built-in date/time functionality.
 # - 24-hour clock.
-#   - Hours: 0-23; rolls over to 0 at 24 hours
-#   - Minutes: 0-59
 #
 # Behaviors:
 # - ::new(hours, minutes):  values as integers; minutes optional
 # - ::at(hours, minutes): self-instantiate, forward args
 #   - `Clock` needs to be stateful to enable `to_s`, `+`, and `-`.
 # - #+, #-: Add or subtract minutes
-# - #== (equivalence): equal if same hours and minutes
+# - #== (equivalence): equal if same minutes
 # - to_s: format as "hh:mm"
 
 # * Examples/tests *
 # Reviewed test cases and extracted rules and behaviors.
 
 # * Data structure *
-# - Store hours and minutes as private instance attributes.
+# - Store minutes using private instance attribute.
 
 # * Algorithm *
+# MINUTES_PER_DAY = 24 * 60
+# - Instantiate with hours = 0, optional minutes = 0.
+#   - Calculate total minutes from hours and minutes (helper method).
+# - `::at` method for convenient
+#   - Self-instantiate.
+# - #+ and #-: add or subtract minutes
+#   - Add to @total_minutes and set attr.
+# - #==
+#   - Compare total_minutes attrs.
+# - #to_s
+#   - format as "hh:mm"
+#
+# Key helper methods:
+# - total_minutes=(value)
+#   - @total_minutes = value mod (MINUTES_PER_DAY).
+# - time_values
+#   - Calculate { hours: ..., minutes: ... } from total_minutes.
