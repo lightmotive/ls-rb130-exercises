ed4d2463288311ec3112f2c3984b580482be066e
Andre Light <andrelight.al@gmail.com>
2022-09-13 01:53:38 +0000

    Implement Ruby Challenge, Medium 3 (in progress)
    - Expand algorithm to `Clock` class.

--- 07_challenges/medium/clock.rb
+++ 07_challenges/medium/clock.rb
@@ -28,7 +28,7 @@
 # - `::at` method for convenient
 #   - Self-instantiate.
 # - #+ and #-: add or subtract minutes
-#   - Add to @total_minutes and set attr.
+#   - Add to or subtract from @total_minutes and invoke `total_minutes=`.
 # - #==
 #   - Compare total_minutes attrs.
 # - #to_s
@@ -37,5 +37,49 @@
 # Key helper methods:
 # - total_minutes=(value)
 #   - @total_minutes = value mod (MINUTES_PER_DAY).
-# - time_values
+# - breakdown
 #   - Calculate { hours: ..., minutes: ... } from total_minutes.
+
+class Clock
+  HOURS_PER_DAY = 24
+  MINUTES_PER_HOUR = 60
+  MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
+
+  def initialize(hours = 0, minutes = 0)
+    #   - Calculate total minutes from hours and minutes (helper method).
+  end
+
+  def at(hours = 0, minutes = 0)
+    #   - Self-instantiate.
+  end
+
+  def +(other)
+    self.total_minutes += other
+  end
+
+  def -(other)
+    self.+(-other)
+  end
+
+  def ==(other)
+    #   - Compare total_minutes attrs.
+  end
+
+  def to_s
+    #   - format as "hh:mm"
+  end
+
+  protected
+
+  attr_reader :total_minutes
+
+  private
+
+  def total_minutes=(value)
+    #   - @total_minutes = value mod (MINUTES_PER_DAY).
+  end
+
+  def breakdown
+    #   - Calculate { hours: ..., minutes: ... } from total_minutes.
+  end
+end
