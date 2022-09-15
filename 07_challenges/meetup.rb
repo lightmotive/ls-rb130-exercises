b446e59f08ed7d15cc4abf6ad0aebc13797953ac
Andre Light <andrelight.al@gmail.com>
2022-09-15 20:03:23 +0000

    Process Ruby Challenge, Medium 4
    - PED[A]C: Class skeleton with high-level steps.

--- 07_challenges/meetup.rb
+++ 07_challenges/meetup.rb
@@ -3,11 +3,11 @@
 # * Understand *
 # - Input:
 #   - Meetup.new(year_number, month_number)
-#   - Meetup#day(weekday, weekday_occurrence_in_month)
-#     - weekday options (case-insensitive):
+#   - Meetup#day(weekday_name, weekday_occurrence_name)
+#     - weekday_name options (case-insensitive):
 #       %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday]
-#     - weekday_occurrence_in_month options (case-insensitive):
-#       %w[first second third fourth fifth last, teenth]
+#     - weekday_occurrence_name options (case-insensitive):
+#       %w[first second third fourth fifth last teenth]
 # - Output:
 #   - `nil` if there are no matching days
 #   - Otherwise, first matching date as Date.new(year, month, month_day)
@@ -18,11 +18,43 @@
 # * Data structure *
 # - Organize a date range into a structure we can filter by Year and
 #   Month (Date Range), then:
-#   - group by weekday (Hash), then:
+#   - group by weekday_name (Hash), then:
 #     - sort by month day, count (e.g., first Monday), and retrieve the date
 #       at a specific count (Array).
-#
-# * Algorithm *
-=begin
 
-=end
+class Meetup
+  WEEKDAY_NAMES = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday].freeze
+  WEEKDAY_OCCURRENCE_NAMES = %w[first second third fourth fifth last teenth].freeze
+
+  attr_reader :year, :month
+
+  def initialize(year, month)
+    @year = year
+    @month = month
+  end
+
+  def day(weekday_name, weekday_occurrence_name)
+    # - Rules:
+    #   - Name inputs should be insensitive to case.
+    #   - Raise an exception if inputs don't match associated options
+    #     (minor UX improvement).
+
+    # * Data structure *
+    # - Organize a date range into a structure we can filter by Year and
+    #   Month (Date Range), then:
+    #   - group by weekday_name (Hash), then:
+    #     - sort by month day, count (e.g., first Monday), and retrieve the date
+    #       at a specific count (Array).
+
+    # * Algorithm *
+    # - `#month_days`: return date range breakdown for given year and month
+    #   as described in data structure section.
+    #   - Filter that breakdown by provided `weekday_name`
+    #     - Filter that selection by provided `weekday_occurrence_name`
+
+    #   - Find up to 1 matching day:
+    #     - Return `nil` if no matching day is found.
+    #     - Otherwise, return first matching date as
+    #       Date.new(year, month, month_day).
+  end
+end
