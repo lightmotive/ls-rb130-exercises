dd7369dbe66781cbeb291bf11010426ee67015d4
Andre Light <andrelight.al@gmail.com>
2022-10-03 01:30:53 +0000

    Implement "batch mode" to enhance performance
    - Now, both `RobotNames#use!` and `RobotNames#release!` are fast.

--- 07_challenges/medium/robot_scalable.rb
+++ 07_challenges/medium/robot_scalable.rb
@@ -1,25 +1,81 @@
 # frozen_string_literal: true
 
+# This is fully scalable alternative to the `robot_alt.rb` implementation.
+# The implementation includes Robot::batch_init that forwards a block to
+# RobotNames#batch_use!, which then:
+# - Activates batch mode, which changes the class behavior to simply append
+#   to `names_used` without sorting.
+#   - Appending a used name to `names_used` in sorted order slows the process
+#     considerably, even when using a binary search algorithm.
+# - Yields to a block that can instantiate multiple Robots quickly.
+# - Deactivates batch mode.
+# - Sorts `names_used` in place.
+#   - Sorting a 670K-element array once is much faster than sequentially
+#     inserting 670K elements into the correct sorted position.
+
+# Compare the following performance with the benchmarks indicated in
+# ./robot_alt.rb. Note that this test setup is slightly different: it uses
+# `Robot::batch_init { ... }` to activate "batch mode" in an encapsulated way.
+
+# # ** Test setup **
+# - Note `Robot.batch_init { ... }` usage.
+#
+# Init performance:
+# init_start_time = Time.now
+#
+# ...require robot_scalable.rb here...
+#
+# puts "Init time: #{Time.now - init_start_time}"
+#
+# robots = []
+# generate_count = 676_000
+#
+# Create performance
+# create_start_time = Time.now
+# puts "Generating #{generate_count} robots..."
+#
+# Robot.batch_init do
+#   robots.push(Robot.new) while robots.size < generate_count
+# end
+#
+# create_seconds = Time.now - create_start_time
+# puts "Generated #{robots.size} robots in #{create_seconds} seconds (~#{robots.size.fdiv(create_seconds).floor}/sec)"
+# puts robots.map(&:name).uniq.size
+#
+# Reset performance
+# reset_start_time = Time.now
+# robots.shuffle!
+# reset_count = 20_000
+#
+# puts "Resetting #{reset_count} robots..."
+# reset_count.times { robots.shift.reset }
+#
+# reset_seconds = Time.now - reset_start_time
+# puts "Reset #{reset_count} robots in #{reset_seconds} seconds (~#{reset_count.fdiv(reset_seconds).floor}/sec)"
+
 # ** robot_scalable.rb **
 # Create performance:
-#   Init (generate and shuffle all possible names): ~0.63 seconds
-#   Generated 676,000 robots in ~22 seconds
-#   Avg. robots/second: ~30,000
+#   Init (generate and shuffle all possible names): ~0.64 seconds
+#   Generated 676,000 robots in ~0.45 seconds (~1,500,000/sec)
 #   - The ~0.64 second startup performance penalty yields vastly improved
 #     generation time and consistent performance regardless of the number of
 #     active robots.
+#   - It's interesting that this is slightly slower than the ./robot_alt.rb
+#     implementation because it has to check `is_batch_mode` with each `use!`
+#     invocation. The different is insignificant enough to ignore in this case.
 # Reset performance:
-#   Reset 200 robots in ~4.5 seconds
-#   Avg. reset robots/second: ~44
-#   - Resetting robots is somewhat slow because it uses **Array#delete* instead
-#     of a `bsearch` implementation. See ./robot_scalable.rb, which solves that
-#     scalability problem.
+#   Reset 20,000 robots in ~2.5 seconds (~8000/sec)
+#   - Now that we're using a fast name distribution algorithm and binary search
+#     for tracking used names, both batch initialization and batch resetting
+#     are fast.
 
 # Generate and track usage of Robot names matching /\A[A-Z]{2}\d{3}\z/.
 #
 # Public behaviors:
 # - `#use!`: shift a name from a pre-shuffled list of available names, then
 #   block that name from further use until released.
+# - `#batch_use!`: same as `#use!`, but internally optimized for batched
+#   invocation.
 # - `#release!(name)`: release a previously selected name; returns `self`.
 #
 # Possible name permutations:
@@ -29,26 +85,45 @@
 class RobotNames
   def initialize
     initialize_names!
+    @is_batch_mode = false
   end
 
   def use!
     raise StandardError, 'All names are in use' if names_available.empty?
 
-    use_name!
+    name = names_available.shift
+
+    if is_batch_mode then names_used << name
+    else
+      insert_before_idx = names_used.bsearch_index { |used| used > name } ||
+                          names_used.size
+      names_used.insert(insert_before_idx, name)
+    end
+
+    name
+  end
+
+  def batch_use!
+    raise StandardError, 'Provide a block that will invoke `use!` multiple times' unless block_given?
+
+    @is_batch_mode = true
+    yield
+    @is_batch_mode = false
+    names_used.sort!
   end
 
   def release!(name)
-    delete_at_idx = names_used_idx(name)
+    delete_at_idx = names_used.bsearch_index { |used| name <=> used }
     return if delete_at_idx.nil?
 
     released_name = names_used.delete_at(delete_at_idx)
-    names_available.push(released_name)
+    names_available << released_name
     self
   end
 
   private
 
-  attr_reader :names_available, :names_used
+  attr_reader :names_available, :names_used, :is_batch_mode
 
   def initialize_names!
     name_possibilities = letter_digit_sequences(2, 3)
@@ -71,19 +146,6 @@ class RobotNames
   def char_permutations(char_range, length)
     char_range.to_a.repeated_permutation(length).to_a.uniq
   end
-
-  def use_name!
-    name = names_available.shift
-    insert_before_idx =
-      names_used.bsearch_index { |used| used > name } ||
-      names_used.size
-    names_used.insert(insert_before_idx, name)
-    name
-  end
-
-  def names_used_idx(name)
-    names_used.bsearch_index { |used| name <=> used }
-  end
 end
 
 # Robot with a randomly generated and guaranteed-unique name with reset
@@ -96,6 +158,13 @@ end
 #   `@name`.
 class Robot
   @@robot_names = RobotNames.new
+
+  def self.batch_init(&block)
+    raise StandardError, "Provide a block that initializes multiple #{name} instances" unless block_given?
+
+    @@robot_names.batch_use!(&block)
+  end
+
   attr_reader :name
 
   def initialize
